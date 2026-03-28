abab6083fff212c356bd4f7d82fc2d8449c671f1
2511500006-Grezelco Govin <2511500006>
2026-03-28 16:47:59 +0700

    perbaikan final

--- jadwalguru.sql
+++ jadwalguru.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 28, 2026 at 08:55 AM
+-- Generation Time: Mar 28, 2026 at 09:44 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -29,20 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tabel_user` (
   `id_user` int(11) NOT NULL,
-  `Username` varchar(50) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Role` enum('admin','guru','siswa') NOT NULL
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('admin','guru','siswa') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tabel_user`
 --
 
-INSERT INTO `tabel_user` (`id_user`, `Username`, `Password`, `Role`) VALUES
-(1, 'admin', '', 'admin'),
-(2, 'admin', '12345', 'admin'),
-(4, 'admin', 'govin', 'admin'),
-(5, '', '', 'admin');
+INSERT INTO `tabel_user` (`id_user`, `username`, `password`, `role`) VALUES
+(11, 'siswa', '123', 'siswa'),
+(12, 'admin', '123', 'admin'),
+(13, 'guru', '123', 'guru');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +61,7 @@ ALTER TABLE `tabel_user`
 -- AUTO_INCREMENT for table `tabel_user`
 --
 ALTER TABLE `tabel_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
